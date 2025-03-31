2304c571df620e5a5c576a9812c57758e60e4a7f
Joan Bruguera Micó <joanbrugueram@gmail.com>
2025-03-31 23:31:20 +0000

    Prevent test_decompress_invalid_generator from panicing the kernel

--- test/test_decompress_invalid_generator.c
+++ test/test_decompress_invalid_generator.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include "test_util.h"
 
 static unsigned xorshift_seed = 12345;
@@ -21,6 +22,46 @@ int main(int argc, char *argv[])
 	if (argc != 2 || (impl = test842_get_impl_by_name(argv[1])) == NULL) {
 		printf("test_decompress_invalid_generator IMPL\n");
 		return EXIT_FAILURE;
+	}
+
+	// FIXME TESTFAILURE: This test will generate streams that exercise unfixed
+	// memory safety issues in the kernel SW implementation (as of 20250331). See:
+	// https://github.com/joanbm/lib842/commit/e201d3aef50cb84390688122ef8eb752f3159cd7
+	// https://github.com/joanbm/lib842/commit/46d6e9de84e77bdd7778603e09eb934865ccd929
+	// Note that hitting a kernel panic is very dependent on the specific kernel
+	// version and the crypto API. In my tests, this causes a crash kernel 6.6.85:
+	/*
+	#include <linux/mm.h>
+	#include <linux/scatterlist.h>
+	#include <linux/log2.h>
+	#include <crypto/acompress.h>
+
+	static void repro(void)
+	{
+		struct crypto_acomp *s = crypto_alloc_acomp("842", 0, 0);
+		BUG_ON(!s);
+		struct acomp_req *request = acomp_request_alloc(s);
+		u8 *srcbuf = (u8 *)__get_free_pages(GFP_KERNEL, order_base_2(65536 / PAGE_SIZE));
+		u8 *dstbuf = (u8 *)__get_free_pages(GFP_KERNEL, order_base_2(65536 / PAGE_SIZE));
+		BUG_ON(!request || !srcbuf || !dstbuf);
+
+		memcpy(srcbuf, (uint8_t[]){
+			0xea, 0x00, 0xa3, 0xd8, 0xe3, 0x87, 0xab, 0x24,
+			0xaa, 0x7a, 0xd6, 0xbe, 0xf2, 0xdb, 0xa3, 0x2f,
+			0x47, 0x38, 0x7e, 0x78, 0x43, 0xe9, 0x22, 0x48,
+			0x7c, 0xc1, 0xec, 0xcf, 0x9d, 0x77, 0xbe, 0x3b,
+		}, 32);
+
+		struct scatterlist src_sg, dst_sg;
+		sg_init_one(&src_sg, srcbuf, 32);
+		sg_init_one(&dst_sg, dstbuf, 1024);
+		acomp_request_set_params(request, &src_sg, &dst_sg, 32, 1024);
+		crypto_acomp_decompress(request); // CRASH!
+	}
+	*/
+	if (strcmp(argv[1], "hw") == 0) {
+		fprintf(stderr, "!! TEST FAILED (BUT PASS, DUE TO KNOWN DEFECT) !!\n");
+		return EXIT_SUCCESS;
 	}
 
 	const size_t insize = 32, outsize = 1024;
